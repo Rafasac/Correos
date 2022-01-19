488b5b99fa9b3ce0b09623495a3f3367260813ae
Rafael Santiago Cruz <rafaelsantiagocruz@outlook.com>
2022-01-18 18:52:22 -0600

    Enero 2022

---
+++ Correos/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace Correos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnAbrirArchivo = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnVerContactos = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripVelocidadWeb = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.cbbDominioOrigen = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(192, 137);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Procesar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(449, 112);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(319, 298);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // btnAbrirArchivo
+            // 
+            this.btnAbrirArchivo.Location = new System.Drawing.Point(12, 47);
+            this.btnAbrirArchivo.Name = "btnAbrirArchivo";
+            this.btnAbrirArchivo.Size = new System.Drawing.Size(75, 23);
+            this.btnAbrirArchivo.TabIndex = 7;
+            this.btnAbrirArchivo.Text = "Abrir CSV";
+            this.btnAbrirArchivo.UseVisualStyleBackColor = true;
+            this.btnAbrirArchivo.Click += new System.EventHandler(this.btnAbrirArchivo_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(795, 112);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(368, 298);
+            this.richTextBox2.TabIndex = 8;
+            this.richTextBox2.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(446, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Correos del archivo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(792, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Nuevos contactos";
+            // 
+            // btnVerContactos
+            // 
+            this.btnVerContactos.Location = new System.Drawing.Point(1088, 83);
+            this.btnVerContactos.Name = "btnVerContactos";
+            this.btnVerContactos.Size = new System.Drawing.Size(75, 23);
+            this.btnVerContactos.TabIndex = 11;
+            this.btnVerContactos.Text = "Ver contactos";
+            this.btnVerContactos.UseVisualStyleBackColor = true;
+            this.btnVerContactos.Click += new System.EventHandler(this.btnVerContactos_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripVelocidadWeb,
+            this.toolStripLabel2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1215, 25);
+            this.toolStrip1.TabIndex = 12;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripVelocidadWeb
+            // 
+            this.toolStripVelocidadWeb.Name = "toolStripVelocidadWeb";
+            this.toolStripVelocidadWeb.Size = new System.Drawing.Size(85, 22);
+            this.toolStripVelocidadWeb.Text = "Velocidad Web";
+            this.toolStripVelocidadWeb.Click += new System.EventHandler(this.toolStripVelocidadWeb_Click);
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(90, 22);
+            this.toolStripLabel2.Text = "Contactos chat ";
+            // 
+            // cbbDominioOrigen
+            // 
+            this.cbbDominioOrigen.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbbDominioOrigen.FormattingEnabled = true;
+            this.cbbDominioOrigen.Items.AddRange(new object[] {
+            "gruposte.com",
+            "equipodeproteccion.com"});
+            this.cbbDominioOrigen.Location = new System.Drawing.Point(89, 93);
+            this.cbbDominioOrigen.Name = "cbbDominioOrigen";
+            this.cbbDominioOrigen.Size = new System.Drawing.Size(178, 21);
+            this.cbbDominioOrigen.TabIndex = 13;
+            this.cbbDominioOrigen.SelectedIndexChanged += new System.EventHandler(this.cbbDominioOrigen_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Dominio";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkCyan;
+            this.ClientSize = new System.Drawing.Size(1215, 438);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbbDominioOrigen);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.btnVerContactos);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.btnAbrirArchivo);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Extraccion de contactos Multinacional de Seguridad Industrial";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnAbrirArchivo;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnVerContactos;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripVelocidadWeb;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ComboBox cbbDominioOrigen;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Correos/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Microsoft.VisualBasic.FileIO;
+using System.IO;
+
+namespace Correos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //mostrar la lista
+            //teniendo el total de iteracciones podemos meter las dos listas en un ciclo for para ir evaluando si el correo existe o no
+            //para ello se tiene que evaluar si el EMAIL existe en la base de datos CORREO tabla CONTACTO
+            //Si existe no hacer nada y si no existe proceder a guardar el NOMBRE y EMAIL en la tabla contacto
+            //para ello se necesita saber el tamaño de la lista para saber el numero de iteraciones que debera realizar
+            for (int c = 0; c < ListaCorreos.Count; c++)
+            {
+                using (SqlConnection cnn = new SqlConnection(sql))
+                {
+                    cnn.Open();
+                    String consulta = "SELECT EMAIL FROM CONTACTOS WHERE EMAIL='"+ListaCorreos[c]+"'";
+                    SqlCommand cmd = new SqlCommand(consulta, cnn);
+
+                    SqlDataReader readers = cmd.ExecuteReader();
+                    if (readers.Read())
+                    {
+                        //Si ya esta registrado el correo no hacer nada 
+                    }
+                    else
+                    {
+                        //insertar el registro en la base de datos 
+                        using (SqlConnection cnnI = new SqlConnection(sql))
+                        {
+                            cnnI.Open();
+                            String insertar = "INSERT INTO CONTACTOS (NOMBRE, EMAIL, FECHA, fkORIGENDOMINIO) VALUES (@NOMBRE, @EMAIL, @FECHA, @DOMINIO)";
+                            SqlCommand cmdI = new SqlCommand(insertar, cnnI);
+                            cmdI.Parameters.Add(new SqlParameter("@NOMBRE", SqlDbType.VarChar)).Value = ListaNombres[c];
+                            cmdI.Parameters.Add(new SqlParameter("@EMAIL", SqlDbType.VarChar)).Value = ListaCorreos[c];
+                            cmdI.Parameters.Add(new SqlParameter("@FECHA", SqlDbType.Date)).Value = System.DateTime.Now.ToString();
+                            cmdI.Parameters.Add(new SqlParameter("@DOMINIO", SqlDbType.Int)).Value = DominoOrigen;
+                            if (cmdI.ExecuteNonQuery() == 1)
+                            {
+
+                                richTextBox2.Text = richTextBox2.Text + ListaNombres[c] + " , " + ListaCorreos[c] + "\n";
+                            }
+                            else
+                            {
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnAbrirArchivo_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+                }
+            }
+
+            richTextBox1.Text = fileContent;
+            RutaDeArchivo = filePath.ToString();
+
+            //Separar Nombre y Email en listas diferentes
+            using (TextFieldParser parser = new TextFieldParser(@"" + RutaDeArchivo))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+                parser.HasFieldsEnclosedInQuotes = true;
+                while (!parser.EndOfData)
+                {
+                    //Console.WriteLine("Linea:");
+                    var fields = parser.ReadFields();
+                    foreach (var field in fields)
+                    {
+                        if (campo == true)
+                        {
+                            campo = false;
+                            ListaNombres.Add(field);
+                        }
+                        else
+                        {
+                            campo = true;
+                            ListaCorreos.Add(field);
+                        }
+                    }
+                }
+            }
+        }
+        private string sql = @"server=192.168.2.20; database=CORREO ; integrated security = true";
+        private bool campo = true;
+        private int DominoOrigen = 0;
+        List<string> ListaNombres = new List<string>();
+        List<string> ListaCorreos = new List<string>();
+        private String RutaDeArchivo = "";
+
+        private void toolStripVelocidadWeb_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnVerContactos_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbbDominioOrigen_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbbDominioOrigen.Text=="equipodeproteccion.com")
+            {
+                DominoOrigen = 1;
+            }else
+            if (cbbDominioOrigen.Text == "gruposte.com")
+            {
+                DominoOrigen = 2;
+            }else
+            if (cbbDominioOrigen.Text == "cursodeprimerosauxilios.com.mx")
+            {
+                DominoOrigen = 4;
+            }
+        }
+    }
+}
